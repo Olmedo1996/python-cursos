e8dac36a1fb792b2de1669819d651fbb029e205e
diego <diegoolmedog96@gmail.com>
2024-08-24 23:19:19 -0400

    feat: tuplas

---
+++ tuplas.py
@@ -0,0 +1,16 @@
+# TUPLA una coleccion ordenada e inmutable de datos
+tupla = ("a", "b", "c")
+vehiculos = ("moto", "auto", "camionetas", "barcos", "bicicleta")
+
+longitud = len(vehiculos)
+print(longitud)
+
+#puede tener distintos tipos de datos number bool o string o anidadas.
+
+#tambien igual que listas
+print(vehiculos[1])
+print(vehiculos[-1])
+
+# ...
+
+
